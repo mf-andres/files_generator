37db66f3a78416654c95bd6f10bc0bdfe81ab01b
amunoz <munozfernandezandres@gmail.com>
2022-01-31 12:53:05 +0100

    feat: item to object converter returns converter with proper file contents

--- tests/converters_generator/unit/test_item_to_object_converter.py
+++ tests/converters_generator/unit/test_item_to_object_converter.py
@@ -6,5 +6,20 @@ def test_returns_converter_with_proper_file_name():
     class_file_contents = class_file_contents_mother.get_class_file_contents()
     converter_generator = ItemToObjectConverterGenerator()
     converter = converter_generator.generate(class_file_contents)
-    converter_has_proper_file_name = converter.file_name == "item_to_diagnosis_converter.py"
+    converter_has_proper_file_name = converter.file_name == "item_to_example_converter.py"
     assert converter_has_proper_file_name
+
+
+def test_returns_converter_with_proper_file_contents():
+    class_file_contents = class_file_contents_mother.get_class_file_contents()
+    converter_generator = ItemToObjectConverterGenerator()
+    converter = converter_generator.generate(class_file_contents)
+    expected_converter_file_contents = """def convert(example_item: ExampleItem) -> Example:
+    return Example(
+        example_item.int_,
+        example_item.float_,
+        example_item.str_,
+        example_item.list_,
+    )"""
+    converter_has_proper_file_contents = converter.file_contents == expected_converter_file_contents
+    assert converter_has_proper_file_contents
--- tests/converters_generator/utils/class_file_contents_mother.py
+++ tests/converters_generator/utils/class_file_contents_mother.py
@@ -1,16 +1,15 @@
 def get_class_file_contents():
     return """
-import datetime
-
-from taupatias_back.diagnosis_prediction.domain.patient_data import PatientData
-
-
-class Diagnosis:
-    def __init__(self, id_: str, patient_data: PatientData, diagnosis: str, comments: str,
-                 creation_date: datetime.datetime):
-        self.id_ = id_
-        self.patient_data = patient_data
-        self.diagnosis = diagnosis
-        self.comments = comments
-        self.creation_date = creation_date
+class Example:
+    def __init__(
+        self, 
+        int_: int, 
+        float_: float, 
+        str_: str,
+        list_: List[int],
+    ):
+        self.int_ = int_
+        self.float_ = float_
+        self.str_ = str_
+        self.list_ = list_
     """
