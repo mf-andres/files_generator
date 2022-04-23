a63e2e162624fd57c310f5e3169a5afae58bd908
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2022-04-23 20:55:17 +0200

    fix: extend underscore process to an infinite number of sections

--- files_generator/domain/utils/name_underscorer.py
+++ files_generator/domain/utils/name_underscorer.py
@@ -1,11 +1,7 @@
+import re
+
+
 def underscore_name(name: str):
-    uppercase_indexes = [index for index, char in enumerate(name) if char.isupper()]
-    uppercase_indexes.pop(0)
-    underscored_name = name
-    for uppercase_index in uppercase_indexes:
-        underscored_name = (
-                underscored_name[:uppercase_index]
-                + "_"
-                + underscored_name[uppercase_index:]
-        )
+    uppercase_slices = re.findall('[A-Z][^A-Z]*', name)
+    underscored_name = "_".join(uppercase_slices)
     return underscored_name
